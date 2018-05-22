55a63efe6105e66eb67ed599a45f00e493211f0b
1511104848 <1511104848@qq.com>
2018-05-22 23:29:25 +0800

    1.多节点注册中心调用

--- src/main/java/com/myspringboot/web/HelloController.java
+++ src/main/java/com/myspringboot/web/HelloController.java
@@ -2,11 +2,11 @@ package com.myspringboot.web;
 
 
 import com.netflix.discovery.converters.Auto;
+import org.apache.logging.log4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.client.hypermedia.DiscoveredResource;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,15 +15,16 @@ import java.util.List;
 
 @RestController
 public class HelloController {
+    private  final org.slf4j.Logger logger = LoggerFactory.getLogger(getClass());
     @Autowired
     private DiscoveryClient discoveryClient;
     @RequestMapping("/hello")
     public String hello(){
-        List<ServiceInstance> list = discoveryClient.getInstances("STORES");
-        for (ServiceInstance instance: list
-             ) {
-            System.out.println("host: "+instance.getHost() +" service_id:"+ instance.getServiceId());
-        }
+        discoveryClient.getServices().forEach(id -> {
+            discoveryClient.getInstances(id).forEach(instance -> {
+                logger.info(", host:" + instance.getHost() + ", service_id:" + instance.getServiceId());
+            });
+        });
         return "hello world";
     }
 
